424f95725146cf46963773f89017357d3f537fb7
fgvieira <1151762+fgvieira@users.noreply.github.com>
2025-10-16 11:25:26 +0200

    Add tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,18 +3,23 @@ from snakemake_interface_scheduler_plugins.base import SchedulerBase
 from snakemake_interface_scheduler_plugins.settings import SchedulerSettingsBase
 from snakemake_interface_scheduler_plugins.tests import TestSchedulerBase
 
+from snakemake_scheduler_plugin_firstfit import Scheduler, SchedulerSettings
 
-class TestGreedyScheduler(TestSchedulerBase):
+
+class FirstFitScheduler(TestSchedulerBase):
     # This ensures that the tests from the base class are executed.
     # Set to False if you want to implement intermediate base classes.
     __test__ = True
 
     def get_scheduler_cls(self) -> Type[SchedulerBase]:
         # Return the scheduler class of your plugin.
-        ...
+        return Scheduler
 
     def get_scheduler_settings(self) -> Optional[SchedulerSettingsBase]:
         # Return the SchedulerSettings instance you want to test.
         # Note that you can put here multiple classes inheriting from TestSchedulerBase
         # or from each other to test different settings.
-        ...
+        return SchedulerSettings(
+            greediness=0.5,
+            omit_prioritize_by_temp_and_input=True,
+        )
